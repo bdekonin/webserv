a573986be9906460ec14a8ddd7a25a085cc99fc2
Bob <bdekonin@student.codam.nl>
2022-08-18 21:02:10 +0200

    favicon working. understanding parsing now

--- server.cpp
+++ server.cpp
@@ -6,7 +6,22 @@
 #include <netinet/in.h>
 #include <string.h>
 
+# include <iostream>
+# include <iomanip>
+# include <sstream>
+# include <fstream>
+# include <string>
+# include <limits>
+# include <cstdio>
+
 #define PORT 8080
+
+#define main_page "HTTP/1.1 200 OK\nContent-Type:text/html\nContent-Length: 95\n\n<h1>Hello</h1><p>&nbsp;</p><p><a href='link_1'>Link 1</a></p><p><a href='link_2'>Link 2</a></p>"
+
+#define link_1 "HTTP/1.1 200 OK\nContent-Type:text/html\nContent-Length: 15\n\n<h1>Link 1</h1>"
+#define link_2 "HTTP/1.1 200 OK\nContent-Type:text/html\nContent-Length: 15\n\n<h1>Link 2</h1>"
+
+#define favi "HTTP/1.1 200 OK\nContent-Type:image/x-icon\nContent-Length: 5430\n\n"
 int main(int argc, char const *argv[])
 {
     int server_fd, new_socket; long valread;
@@ -48,16 +63,70 @@ int main(int argc, char const *argv[])
             perror("In accept");
             exit(EXIT_FAILURE);
         }
-
-
-
-
         char buffer[30000] = {0};
-        valread = read( new_socket , buffer, 30000);
-        printf("%s\n",buffer );
-		
-        write(new_socket , hello , strlen(hello));
-        printf("------------------Hello message sent-------------------\n");
+
+
+		valread = recv( new_socket , buffer, 30000, 0);
+
+        // parse request-line
+        std::string method, uri, version;
+
+        sscanf(buffer, "%s %s %s", method, uri, version);
+
+        
+
+		// process
+				if (buffer[10] == '1')
+		{
+			// do link_1
+			char *arr= strdup(link_1);
+
+			send(new_socket , arr , strlen(arr) , 0 );
+
+		}
+		else if (buffer[10] == '2')
+		{
+			// do link_2
+			char *arr= strdup(link_2);
+
+			send(new_socket , arr , strlen(arr) , 0 );
+		}
+        else if (buffer[4] == '/' && buffer[5] == 'f')
+        {
+            std::ifstream file;
+            std::stringstream	buffer;
+
+            file.open("/mnt/c/Users/Bob/Documents/webserv/favicon.ico", std::ifstream::in);
+
+            buffer << file.rdbuf();
+
+            std::string str = favi + buffer.str();
+
+            char *arr = strdup(str.c_str());
+            // print all variables
+            std::cout << "buffer: [" << buffer.str() << "]" << std::endl;
+            // size of buffer
+            std::cout << "size of buffer: " << buffer.str().size() << std::endl;
+        
+
+
+			send(new_socket , str.c_str() , strlen(favi) + buffer.str().size() , 0 );
+        }
+		else
+		{
+			char *arr= strdup(main_page);
+
+			send(new_socket , arr , strlen(arr) , 0 );
+		}
+
+
+
+
+        // valread = read( new_socket , buffer, 30000);
+        printf("\n%s\n", buffer);
+
+        // write(new_socket , hello , strlen(hello));
+        // printf("------------------Hello message sent-------------------\n");
         close(new_socket);
     }
     return 0;
