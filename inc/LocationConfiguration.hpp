ed70ecc3a0fcc7c489ddbf62644483c3206a8b04
rkieboom <rowankie37@outlook.com>
2022-11-05 13:49:54 +0100

    Moved header functions to file

--- inc/LocationConfiguration.hpp
+++ inc/LocationConfiguration.hpp
@@ -6,7 +6,7 @@
 /*   By: bdekonin <bdekonin@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/08/20 21:42:51 by bdekonin      #+#    #+#                 */
-/*   Updated: 2022/11/04 11:58:59 by bdekonin      ########   odam.nl         */
+/*   Updated: 2022/11/04 21:50:38 by rkieboom      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,76 +21,29 @@ class LocationConfiguration : public Configuration
 {
 	public:
 		/* Constructor  */
-		LocationConfiguration()
-		{
-		}
-		LocationConfiguration(std::string &s)
-		: _path(s)
-		{
-		}
-
+		LocationConfiguration();
+		LocationConfiguration(std::string &s);
 		/* Destructor */
-		virtual ~LocationConfiguration()
-		{
-			this->clear();
-		}
+		virtual ~LocationConfiguration();
 
 		/* Copy constructor */
-		LocationConfiguration(const LocationConfiguration *src)
-		: Configuration()
-		{
-			*this = *src;
-		}
-		LocationConfiguration(const LocationConfiguration &src)
-		: Configuration()
-		{
-			*this = src;
-		}
+		LocationConfiguration(const LocationConfiguration *src);
+		LocationConfiguration(const LocationConfiguration &src);
 
 
 		/* Operation overload = */
-		LocationConfiguration& operator = (const LocationConfiguration& src)
-		{
-			this->_error_page = src.get_error_page();
-			this->_client_max_body_size = src.get_client_max_body_size();
-			this->_methods[0] = src.get_methods(0);
-			this->_methods[1] = src.get_methods(1);
-			this->_methods[2] = src.get_methods(2);
-			this->_return = src.get_return();
-			this->_root = src.get_root();
-			this->_autoindex = src.get_autoindex();
-			this->_index = src.get_index();
-			this->_cgi = src.get_cgi();
-			this->_path = src.get_path(); // TODO check if this is correct
-			return *this;
-		}
+		LocationConfiguration& operator = (const LocationConfiguration& src);
 
 		// Methods
-		void clean()
-		{
-			Configuration::clear();
-			this->_path.clear();
-		}
+		void clean();
 
 		// Setters
-		void set_path(std::string &s)
-		{
-			this->_path = s;
-		}
-		void set_path(std::string const &s)
-		{
-			this->_path = s;
-		}
+		void set_path(std::string &s);
+		void set_path(std::string const &s);
 
 		// Getters
-		std::string			&get_path()
-		{
-			return (this->_path);
-		}
-		const std::string	&get_path() const
-		{
-			return (this->_path);
-		}
+		std::string			&get_path();
+		const std::string	&get_path() const;
 	private:
 		std::string		_path;
 };
